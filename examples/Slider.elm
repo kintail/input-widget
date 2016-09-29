b527dd7bfd3a0c9eafbf8d7f7082b6d799e4db61
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-09-29 12:36:15 +1000

    Add slider example

---
+++ examples/Slider.elm
@@ -0,0 +1,22 @@
+module Slider exposing (..)
+
+import Kintail.InputWidget as InputWidget exposing (InputWidget)
+import Html exposing (Html)
+import Html.App as Html
+
+
+view : Float -> Html Float
+view value =
+    Html.div []
+        [ InputWidget.slider [] { min = 0, max = 5, step = 0.1 } value
+        , Html.text (toString value)
+        ]
+
+
+main : Program Never
+main =
+    Html.beginnerProgram
+        { model = 1.0
+        , update = always
+        , view = view
+        }
